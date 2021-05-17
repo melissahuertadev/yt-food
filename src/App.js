d1100da7891f39fcfc2c6ebdf3269d023e43b382
piratelicorne <thecodeloops@gmail.com>
2021-05-17 00:10:35 -0500

    add index app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import SearchBar from "./components/searchbar";
+import youtube from "./apis/youtube";
+import VideoList from "./components/videolist";
+import VideoDetail from "./components/videodetail";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+      layout: "left",
+    };
+  }
+
+  componentDidMount(){
+    this.onTermSubmit('peruvian');
+  }
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", { params: { q: `${term}` } });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+      <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="ten wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
